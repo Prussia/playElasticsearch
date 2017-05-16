e24d9d4d0c2bdec67701a34cf4b7ac72ee11e2bd
Prussia <prussia.hu@pwc.com>
2017-05-16 11:37:05 +0800

    Update

--- create_elasticsearch.sh
+++ create_elasticsearch.sh
@@ -1,1 +1,8 @@
 docker run -d --name elasticsearch --restart=on-failure:5 -p 9200:9200 -p 9300:9300  elasticsearch:1.7.5
+
+docker exec -it elasticsearch bash
+
+plugin -i elasticsearch/license/1.0.0 
+
+plugin -i elasticsearch/marvel/latest
+
